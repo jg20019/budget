a0e637058958d188e13c70da711dd6703614963d
John Gibson <jg20019@gmail.com>
2023-04-13 18:34:22 -0400

    feat!: Add Inputs

---
+++ src/lib/NumberInput.svelte
@@ -0,0 +1,31 @@
+<script>
+import TextInput from "./TextInput.svelte";
+import { createEventDispatcher } from 'svelte'
+
+const dispatch = createEventDispatcher()
+
+let input
+
+function onInput(e)
+{
+    const inputVal = e.detail.value.trim()
+    const amount = parseFloat(inputVal)
+    if (isNaN(amount)) {
+        dispatch('input', {
+            value: 0,
+            error: 'Please enter a number'
+        })
+    } else {
+        dispatch('input', {
+            value: amount,
+            error: ''
+        })
+    }
+}
+</script>
+
+<TextInput 
+  bind:value="{input}"
+  on:input={onInput}
+/>
+
---
+++ src/lib/TextInput.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+
+    export let value = ''
+    const dispatch = createEventDispatcher()
+
+    function onInput()
+    {
+        dispatch('input', { value })
+    }
+</script>
+
+<input class="bg-gray-200 pl-2 appearance-none border-2 
+    border-gray-200 rounded" 
+    type="text" 
+    on:input={onInput} 
+    min=0 
+    bind:value="{value}" 
+/>
