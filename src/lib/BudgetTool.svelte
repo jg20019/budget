23b1bada2443194fbe030b4dbd02911c13b3c3a2
John Gibson <jg20019@gmail.com>
2023-04-13 16:26:43 -0400

    feat: Create initial layout

--- src/lib/BudgetTool.svelte
+++ src/lib/BudgetTool.svelte
@@ -9,7 +9,18 @@ let errors = {
 }
 
 let income = 0
-let expenses = []
+let expenses = [
+    {
+     for: "Car note",
+     amount: 300.0,
+     spent: 150.0
+    },
+    {
+     for: "Gas money",
+     amount: 50.0,
+     spent: 0,
+    }
+]
 
 $: remaining = income - expenses.reduce((total, {amount}) => total + amount, 0)
 $: spent = income - expenses.reduce((total, {spent}) => total + spent, 0)
@@ -28,14 +39,42 @@ function updateIncome(e)
 }
 </script>
 
-<main>
-    <div class="flex items-center"> 
-        <label for="income"> 
-            Income: <input on:input={updateIncome} min=0 bind:value="{form.income}" />
+<main class="container lg mx-auto"> 
+    <h1 class="font-bold text-2xl mt-5"> Make a Budget </h1>
+    <h2 class="font-bold text-xl mt-10 mb-2"> Enter Income </h2>
+    <div> 
+        <label class="text-lg" for="income"> 
+        Income: 
+        <input class="bg-gray-200 pl-2 appearance-none border-2 
+        border-gray-200 rounded" 
+        type="text" 
+        on:input={updateIncome} 
+        min=0 
+        bind:value="{form.income}" 
+        />
         </label>
-        <p> Remaining: { remaining } </p>
-        <p> Spent: { remaining } </p>
+        <p class="text-lg"> Remaining: { remaining } </p>
+        <p class="text-lg"> Spent: { spent } </p>
     </div>
+    <h2 class="font-bold text-xl mt-10 mb-2"> Expenses </h2>
+    <table class="table-auto w-screen">
+        <thead class="text-left">
+            <tr>
+                <th> For </th>
+                <th> Amount </th>
+                <th> Spent </th>
+            </tr>
+        </thead>
+        <tbody>
+            {#each expenses as expense}
+            <tr>
+                <td> {expense.for} </td>
+                <td> {expense.amount} </td>
+                <td> {expense.spent} </td>
+            <tr>
+            {/each}
+        </tbody>
+    </table>
 </main>
 
 <style>
